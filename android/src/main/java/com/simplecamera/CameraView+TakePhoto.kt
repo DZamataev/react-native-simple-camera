bff7ffc19c424fa1a9e763c06f97e61da64ea29b
SergeyMild <sergeymild@yandex.ru>
2023-04-04 09:48:59 +0300

    Revert "save android photo"
    This reverts commit 6b2bdd7bfb24116d33457398b7816d639500c90a.

--- android/src/main/java/com/simplecamera/CameraView+TakePhoto.kt
+++ android/src/main/java/com/simplecamera/CameraView+TakePhoto.kt
@@ -1,15 +1,9 @@
 package com.simplecamera
 
 import android.annotation.SuppressLint
-import android.content.ContentValues
-import android.graphics.ImageDecoder
 import android.hardware.camera2.*
-import android.os.Build
-import android.os.Environment
-import android.provider.MediaStore
 import android.util.Log
 import androidx.camera.camera2.interop.Camera2CameraInfo
-import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageCapture
 import androidx.camera.core.ImageProxy
 import com.facebook.react.bridge.Arguments
@@ -20,48 +14,6 @@ import com.simplecamera.utils.takePicture
 import kotlinx.coroutines.*
 import java.io.File
 import kotlin.system.measureTimeMillis
-
-
-fun CameraView.getOutputDirectory(): String {
-  return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-    "${Environment.DIRECTORY_DCIM}/SimpleCamera/"
-  } else {
-    "${context.getExternalFilesDir(Environment.DIRECTORY_DCIM)}/SimpleCamera/"
-  }
-}
-
-@SuppressLint("UnsafeOptInUsageError")
-fun CameraView.outputFileOptions(name: String, savePath: String): ImageCapture.OutputFileOptions {
-  val camera2Info = Camera2CameraInfo.from(camera!!.cameraInfo)
-  val lensFacing = camera2Info.getCameraCharacteristic(CameraCharacteristics.LENS_FACING)
-
-  // Setup image capture metadata
-  val metadata = ImageCapture.Metadata().apply {
-    // Mirror image when using the front camera
-    isReversedHorizontal = lensFacing == CameraCharacteristics.LENS_FACING_FRONT
-  }
-  MediaStore.Images.Media.EXTERNAL_CONTENT_URI
-  // Options fot the output image file
-  return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-    val contentValues = ContentValues().apply {
-      put(MediaStore.MediaColumns.DISPLAY_NAME, name)
-      put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
-      put(MediaStore.MediaColumns.RELATIVE_PATH, savePath)
-    }
-
-    val contentResolver = context.contentResolver
-
-    // Create the output uri
-    val contentUri = MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY)
-
-    ImageCapture.OutputFileOptions.Builder(contentResolver, contentUri, contentValues)
-  } else {
-    File(savePath).mkdirs()
-    val file = File(savePath, "${name}.jpg")
-
-    ImageCapture.OutputFileOptions.Builder(file)
-  }.setMetadata(metadata).build()
-}
 
 @SuppressLint("UnsafeOptInUsageError")
 suspend fun CameraView.takePhoto(options: ReadableMap): WritableMap = coroutineScope {
@@ -75,7 +27,6 @@ suspend fun CameraView.takePhoto(options: ReadableMap): WritableMap = coroutineS
     }
   }
 
-
   if (options.hasKey("flash")) {
     val flashMode = options.getString("flash")
     imageCapture!!.flashMode = when (flashMode) {
@@ -86,23 +37,42 @@ suspend fun CameraView.takePhoto(options: ReadableMap): WritableMap = coroutineS
     }
   }
 
+  val camera2Info = Camera2CameraInfo.from(camera!!.cameraInfo)
+  val lensFacing = camera2Info.getCameraCharacteristic(CameraCharacteristics.LENS_FACING)
+
+  val results = awaitAll(
+    async(coroutineContext) {
+      Log.d(CameraView.TAG, "Taking picture...")
+      val startCapture = System.nanoTime()
+      val pic = imageCapture!!.takePicture(takePhotoExecutor)
+      val endCapture = System.nanoTime()
+      Log.i(CameraView.TAG_PERF, "Finished image capture in ${(endCapture - startCapture) / 1_000_000}ms")
+      pic
+    },
+    async(Dispatchers.IO) {
+      Log.d(CameraView.TAG, "Creating temp file...")
+      File.createTempFile("mrousavy", ".jpg", context.cacheDir).apply { deleteOnExit() }
+    }
+  )
+  val photo = results.first { it is ImageProxy } as ImageProxy
+  val file = results.first { it is File } as File
+
+  @Suppress("BlockingMethodInNonBlockingContext")
+  withContext(Dispatchers.IO) {
+    Log.d(CameraView.TAG, "Saving picture to ${file.absolutePath}...")
+    val milliseconds = measureTimeMillis {
+      val flipHorizontally = lensFacing == CameraCharacteristics.LENS_FACING_FRONT
+      photo.save(file, flipHorizontally)
+    }
+    Log.i(CameraView.TAG_PERF, "Finished image saving in ${milliseconds}ms")
+  }
 
   val map = Arguments.createMap()
-  withContext(Dispatchers.IO) {
-    val name = System.currentTimeMillis().toString()
-    val savePath = getOutputDirectory()
+  map.putString("path", file.absolutePath)
+  map.putInt("width", photo.width)
+  map.putInt("height", photo.height)
 
-    val pic = imageCapture!!.takePicture(outputFileOptions(name, savePath), takePhotoExecutor)
-    val uri = pic.savedUri ?: throw java.lang.RuntimeException("errorTakePhoto")
-    val bitmap = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-      ImageDecoder.decodeBitmap(ImageDecoder.createSource(context.contentResolver, uri))
-    } else {
-      MediaStore.Images.Media.getBitmap(context.contentResolver, uri)
-    }
-    map.putInt("width", bitmap.width)
-    map.putInt("height", bitmap.height)
-    map.putString("path", "${savePath}${name}.jpg")
-  }
+  photo.close()
 
   Log.d(CameraView.TAG, "Finished taking photo!")
 
--- android/src/main/java/com/simplecamera/utils/ImageCapture+suspendables.kt
+++ android/src/main/java/com/simplecamera/utils/ImageCapture+suspendables.kt
@@ -2,6 +2,7 @@ package com.simplecamera.utils
 
 import androidx.camera.core.ImageCapture
 import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.ImageProxy
 import java.util.concurrent.Executor
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
@@ -16,6 +17,23 @@ suspend inline fun ImageCapture.takePicture(options: ImageCapture.OutputFileOpti
       }
 
       override fun onError(exception: ImageCaptureException) {
+        cont.resumeWithException(exception)
+      }
+    }
+  )
+}
+
+suspend inline fun ImageCapture.takePicture(executor: Executor) = suspendCoroutine<ImageProxy> { cont ->
+  this.takePicture(
+    executor,
+    object : ImageCapture.OnImageCapturedCallback() {
+      override fun onCaptureSuccess(image: ImageProxy) {
+        super.onCaptureSuccess(image)
+        cont.resume(image)
+      }
+
+      override fun onError(exception: ImageCaptureException) {
+        super.onError(exception)
         cont.resumeWithException(exception)
       }
     }
