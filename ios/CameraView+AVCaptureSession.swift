f85a701ad05b431b93cc5bd6c6048c8417c9eed3
Denis Zamataev <denis.zamataev@gmail.com>
2023-04-14 13:01:23 +0300

    Revert "add delegate declaration"
    This reverts commit 782d27e40204403dec3213b7bb97caad58eaa834.

--- ios/CameraView+AVCaptureSession.swift
+++ ios/CameraView+AVCaptureSession.swift
@@ -5,7 +5,7 @@ import Foundation
 /**
  Extension for CameraView that sets up the AVCaptureSession, Device and Format.
  */
-extension CameraView: AVCaptureMetadataOutputObjectsDelegate {
+extension CameraView {
     // pragma MARK: Configure Capture Session
     
     /**
