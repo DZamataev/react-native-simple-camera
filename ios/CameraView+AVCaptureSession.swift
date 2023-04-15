daa9c9f0ba440f2f6fef3afee6637030ef8b1883
sergeymild <sergeymild@yandex.ru>
2023-04-15 09:52:35 +0300

    fix crash on ios

--- ios/CameraView+AVCaptureSession.swift
+++ ios/CameraView+AVCaptureSession.swift
@@ -61,13 +61,15 @@ extension CameraView {
             }
             captureSession.addInput(videoDeviceInput!)
             
-            // Initialize a AVCaptureMetadataOutput object and set it as the output device to the capture session.
-            let captureMetadataOutput = AVCaptureMetadataOutput()
-            captureSession.addOutput(captureMetadataOutput)
+            if enableReadCode {
+                // Initialize a AVCaptureMetadataOutput object and set it as the output device to the capture session.
+                let captureMetadataOutput = AVCaptureMetadataOutput()
+                captureSession.addOutput(captureMetadataOutput)
 
-            // Set delegate and use the default dispatch queue to execute the call back
-            captureMetadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
-            captureMetadataOutput.metadataObjectTypes = [AVMetadataObject.ObjectType.qr]
+                // Set delegate and use the default dispatch queue to execute the call back
+                captureMetadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+                captureMetadataOutput.metadataObjectTypes = [AVMetadataObject.ObjectType.qr]
+            }
 
         } catch {
             invokeOnError(.device(.invalid))
